2d9a7b71308a6289e3dd0676a7760e37eae44bf5
Tung Tran <tigoilati2706@gmail.com>
2023-10-30 17:06:12 +0700

    up code lab 4

---
+++ lab 3/RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.data.Datasource
+
+
+import com.example.recyclerview.adapter.ItemAdapter
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
+    }
+}
